d3b431e7acdfcbfa107a5fd7268672298c937ca4
Ritesh <ritesh152002@gmail.com>
2021-11-06 23:58:28 +0530

    logo image loc changed

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,11 +5,7 @@ function Navbar() {
   return (
     <nav className="bg-blue-400">
       <div className="flex">
-        <Image
-          src="/logo.png"
-          height={50}
-          width={100}
-        />
+        <Image src="/images/logo.png" height={50} width={100} />
       </div>
     </nav>
   );
