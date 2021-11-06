511b6efaad8a6c9d2e8ab0590ae4a36abad0a548
Ritesh <ritesh152002@gmail.com>
2021-11-06 21:39:56 +0530

    Footer styled

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
   return (
-    <div className="">
+    <div className=" bg-[#F7F6F2] ">
       <div className=" flex ">
-        <div className="max-w-full ">
+        <div className=" ">
           <Image
             src="/icons/phone.png"
             width={500}
@@ -14,7 +15,41 @@ function Footer() {
         </div>
         <div className=" flex items-center ">
           <div className="">
-            <h3 className=" text-3xl font-bold ">Download</h3>
+            <h3 className=" p-4 text-5xl font-semibold tracking-wider leading-5 ">
+              <Link href="/">
+                <a>
+                  <span className=" text-[#077861] text-5xl font-bold ">
+                    Download{" "}
+                  </span>
+                </a>
+              </Link>
+              the app for{" "}
+            </h3>
+            <h3 className=" p-4 pt-2 text-5xl font-semibold tracking-wider leading-5 ">
+              free
+            </h3>
+            <div className="flex items-center">
+              <Link href="/">
+                <a>
+                  <Image
+                    className="transition transform hover:scale-105 "
+                    src="/icons/google-play.png"
+                    width={250}
+                    height={100}
+                  />
+                </a>
+              </Link>
+              <Link href="/">
+                <a>
+                  <Image
+                    className=" pt-0 transition transform hover:scale-105 "
+                    src="/icons/apple.png"
+                    width={250}
+                    height={230}
+                  />
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
