c449e58a9ab5610c7034d6482a8c6c8454940bc1
Ritesh <ritesh152002@gmail.com>
2021-11-07 00:01:09 +0530

    change

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="bg-blue-400 p-2">
       <div className="flex items-center">
-        <Image src="/logo.png" height={50} width={100} />
+        <Image src="/images/logo.png" height={50} width={100} />
         <div className="px-10">
           <button className="box-border py-1 px-2 border-2 rounded-md font-quicksand">
             Medicine
