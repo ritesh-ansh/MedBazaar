4720e16d373f94968eea6f904b8e8de64912b5bd
Tushar Vaishnav <tusharvaishnav21@gmail.com>
2021-11-06 23:57:04 +0530

    tushar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,13 +3,17 @@ import link from "next/link";
 
 function Navbar() {
   return (
-    <nav className="bg-blue-400">
-      <div className="flex">
+    <nav className="bg-blue-400 p-2">
+      <div className="flex items-center">
         <Image
           src="/logo.png"
           height={50}
           width={100}
         />
+        <div className="px-10">
+        <button className="box-border py-1 px-2 border-2 rounded-md font-quicksand">Medicine</button>
+        </div>
+        
       </div>
     </nav>
   );
