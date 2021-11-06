2811a4bd3a2cfdfd1c26beb0672be90f8c9856f8
Ritesh <ritesh152002@gmail.com>
2021-11-06 13:07:56 +0530

    initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="bg-blue-400">
+      <div className="flex justify-center">
+        <h6 className="p-5">MedBazar</h6>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
