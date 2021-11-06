cea8ab261a4d86eb3bfbd3e7b3a0c9c60df3c7cc
Tushar Vaishnav <tusharvaishnav21@gmail.com>
2021-11-06 13:30:44 +0530

    tushar committed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="bg-blue-400  tushy bro">
+    <nav className="bg-blue-400">
       <div className="flex justify-center">
         <h6 className="p-5">MedBazar</h6>
       </div>
