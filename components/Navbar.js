32a250a28fa7c757d318da3d4fa17da911f1576c
Ritesh <ritesh152002@gmail.com>
2021-11-07 13:07:55 +0530

    main search change

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import Search from "./Search";
 
 function Navbar() {
   return (
-    <nav className="bg-blue-400 p-2 rounded-b-md">
+    <nav className="bg-blue-400 p-2 rounded-b-md hidden">
       <div className="flex items-center">
         <Image src="/images/logo.png" height={50} width={100} />
         <div className="px-10">
@@ -19,26 +19,21 @@ function Navbar() {
         <div className="items-center ml-auto flex">
           <Link href="/">
             <a className="transition transform hover:scale-105 mx-5">
-              <Image
-                src="/icons/cart.png"
-                width={25}
-                height={25}
-              />
+              <Image src="/icons/cart.png" width={25} height={25} />
             </a>
           </Link>
           <Link href="/">
             <a className="transition transform hover:scale-105 mx-5">
-              <Image
-                src="/icons/rx.png"
-                width={25}
-                height={25}
-              />
+              <Image src="/icons/rx.png" width={25} height={25} />
             </a>
           </Link>
 
-          <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5 ml-5">Login</button>
-          <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5">SignIn</button>
-
+          <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5 ml-5">
+            Login
+          </button>
+          <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5">
+            SignIn
+          </button>
         </div>
       </div>
     </nav>
