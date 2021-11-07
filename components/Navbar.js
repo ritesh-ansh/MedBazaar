4ebbd9e725d4677843e6ca32a808971ab82fde8b
Tushar Vaishnav <tusharvaishnav21@gmail.com>
2021-11-07 12:43:06 +0530

    j

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,32 +12,33 @@ function Navbar() {
             Medicine
           </button>
         </div>
-
-        <Search/>
+        <div className="ml-auto">
+          <Search />
+        </div>
 
         <div className="items-center ml-auto flex">
-        <Link href="/">
-                <a className="transition transform hover:scale-105 mx-5">
-                  <Image
-                    src="/icons/cart.png"
-                    width={25}
-                    height={25}
-                  />
-                </a>
-              </Link>
-        <Link href="/">
-                <a className="transition transform hover:scale-105 mx-5">
-                  <Image
-                    src="/icons/rx.png"
-                    width={25}
-                    height={25}
-                  />
-                </a>
-        </Link>
+          <Link href="/">
+            <a className="transition transform hover:scale-105 mx-5">
+              <Image
+                src="/icons/cart.png"
+                width={25}
+                height={25}
+              />
+            </a>
+          </Link>
+          <Link href="/">
+            <a className="transition transform hover:scale-105 mx-5">
+              <Image
+                src="/icons/rx.png"
+                width={25}
+                height={25}
+              />
+            </a>
+          </Link>
 
-        <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5 ml-5">Login</button>
-        <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5">SignIn</button>
-  
+          <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5 ml-5">Login</button>
+          <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5">SignIn</button>
+
         </div>
       </div>
     </nav>
--- components/Search.js
+++ components/Search.js
@@ -1,7 +1,11 @@
 function Search() {
     return (
-        <div>
-            
+        <div className="">
+            <input className="w-[50vh] p-1 shadow-md rounded-md border-2 pl-2 py-2 text-sm focus:outline-none focus:border-[#009F7F] hover:border-[#009F7F] focus:ring-[#009F7F]" 
+            placeholder="Search for stores or medicines" 
+            type="text" />
+            <button type="submit" className="bg-green-400 py-2 text-sm rounded-r-md px-3 invisible">Search
+            </button>
         </div>
     )
 }
