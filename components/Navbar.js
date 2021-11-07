09851ab215047bc19f85bdcb0e08c0b7f58b9bb8
Tushar Vaishnav <tusharvaishnav21@gmail.com>
2021-11-07 12:07:26 +0530

    d

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,7 +16,6 @@ function Navbar() {
         <Search/>
 
         <div className="items-center ml-auto flex">
-<<<<<<< HEAD
         <Link href="/">
                 <a className="transition transform hover:scale-105 mx-5">
                   <Image
@@ -39,25 +38,6 @@ function Navbar() {
         <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5 ml-5">Login</button>
         <button className="box-border py-0.5 px-1.5 border-2 rounded-md font-quicksand text-sm mr-5">SignIn</button>
   
-=======
-          <Link href="/">
-            <a className="transition transform hover:scale-105">
-              <Image src="/icons/cart.png" width={25} height={25} />
-            </a>
-          </Link>
-          <Link href="/">
-            <a className="transition transform hover:scale-105">
-              <Image src="/icons/rx.png" width={25} height={25} />
-            </a>
-          </Link>
-
-          <button className="box-border py-1 px-2 border-2 rounded-md font-mulish">
-            Login
-          </button>
-          <button className="box-border py-1 px-2 border-2 rounded-md font-">
-            SignIn
-          </button>
->>>>>>> ff90f0f030e8e9a48ad760eea4f559cc58d5f325
         </div>
       </div>
     </nav>
