2deea70cd052233df2e5d444dcb106a18b9b133e
Ritesh <ritesh152002@gmail.com>
2021-11-29 00:56:41 +0530

    changed colours of buttons in pages

--- components/Search.js
+++ components/Search.js
@@ -1,6 +1,20 @@
+import { useRouter } from "next/router";
+
 function Search({ navBar }) {
+  const router = useRouter();
+  const { page } = router.query;
+
   return (
-    <div className=" flex items-center overflow-hidden hover:scale-105 transition transform duration-75 focus-within:scale-105 hover:border-2 rounded-lg focus-within:border-[#009F7F] focus-within:border-2 hover:border-[#009F7F]">
+    <div
+      className={
+        " flex items-center overflow-hidden hover:scale-105 transition transform duration-75 focus-within:scale-105 hover:border-2 rounded-lg  focus-within:border-2 " +
+        (page === "Beauty"
+          ? " focus-within:border-[#cfc9cd] hover:border-[#cfc9cd] "
+          : page === "Health-tips"
+          ? "focus-within:border-[#939393] hover:border-[#939393] "
+          : " hover:border-[#009F7F] focus-within:border-[#009F7F] ")
+      }
+    >
       <input
         className={
           " p-2 overflow-hidden shadow-md  border-r-0  md:text-md outline-none focus:outline-none" +
@@ -14,10 +28,15 @@ function Search({ navBar }) {
       <button
         type="submit"
         className={
-          " text-white p-2 bg-green-700 outline-none hover:outline-none focus:outline-none md:p-4 border-[#009F7F] hover:border-l-0  hover:ring-[#009F7F]  hover:bg-[#009F7F] m-0   rounded-r-md    " +
+          " text-white p-2  outline-none hover:outline-none focus:outline-none md:p-4  m-0   rounded-r-md    " +
           (navBar
             ? "md:w-[15vh] md:text-sm md:px-2"
-            : "md:w-[20vh] md:text-md md:px-3")
+            : "md:w-[20vh] md:text-md md:px-3") +
+          (page === "Beauty"
+            ? " bg-[#746156] border-[#746156] hover:border-l-0  hover:ring-[#746156]  hover:bg-[#746156]  "
+            : page === "Health-tips"
+            ? " bg-[#939393] border-[#939393] hover:border-l-0  hover:ring-[#939393]  hover:bg-[#939393]  "
+            : " bg-green-700 border-[#009F7F] hover:border-l-0  hover:ring-[#009F7F]  hover:bg-[#009F7F] ")
         }
       >
         Search
